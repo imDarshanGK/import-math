a4b571f3cc6866c252cea8297ee621ed2551581e
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2023-11-08 18:34:55 +0530

    math floor
    math-floor

---
+++ math floor.py
@@ -0,0 +1,11 @@
+import math
+print(math.floor(0.6))
+print(math.floor(11.24))
+print(math.floor(15.3))
+print(math.floor(-5.3))
+print(math.floor(122.622))
+print(math.floor(10.0))
+print(math.floor(111.622))
+print(math.floor(1110.644))
+
+      
